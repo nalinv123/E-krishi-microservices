b12d5c9ed6c5169706190a318a19b65924003899
Nalin Vaidya <nalinvaidya@gmail.com>
2020-02-26 20:10:06 +0700

    Initial Commit

---
+++ src/main/java/com/nvdevelopers/e_krishi/EKrishiApplication.java
@@ -0,0 +1,13 @@
+package com.nvdevelopers.e_krishi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EKrishiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EKrishiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nvdevelopers/e_krishi/EKrishiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nvdevelopers.e_krishi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EKrishiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

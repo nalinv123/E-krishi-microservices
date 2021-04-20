be70b0a43bb1f5afb8b3a93bb7891042abdde095
Nalin Vaidya <nalinvaidya@gmail.com>
2021-04-20 16:13:13 +0700

    Removed log4j2 and added logback as logging framework. Also, updated .gitignore to avoid pushing log files to version control.

--- src/main/java/com/nvdevelopers/e_krishi/EKrishiApplication.java
+++ src/main/java/com/nvdevelopers/e_krishi/EKrishiApplication.java
@@ -3,10 +3,15 @@ package com.nvdevelopers.e_krishi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Locale;
+
 @SpringBootApplication
 public class EKrishiApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnknownHostException {
+		System.setProperty("log.name", "e-krishi-" + InetAddress.getLocalHost().getHostName().toLowerCase(Locale.ROOT));
 		SpringApplication.run(EKrishiApplication.class, args);
 	}
 
